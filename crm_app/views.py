a4d41e42308292d98b4239654f9438f34854ad16
Victor Abuka <vickerdent@gmail.com>
2023-06-12 20:17:44 +0100

    login, logout and register

--- crm_app/views.py
+++ crm_app/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
+
     return render(request, "home.html", {})
+
+def login_user(request):
+    # check if login attempt or normal request
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have logged in successfully.")
+            return redirect("home")
+        else:
+            messages.error(request, "Incorrect username or password.")
+            return redirect("login")
+        
+    return render(request, "login.html", {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You've been logged out successfully.")
+    return redirect("home")
+
+def register(request):
+    return render(request, "register.html", {})
